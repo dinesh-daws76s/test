ce100f094a05509cea8353dcebbd00e81fef0d76
dbramanayanam <152831955+dbramanayanam@users.noreply.github.com>
2024-11-20 14:32:30 +0400

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,15 +5,17 @@ terraform {
       version = "5.43.0"
     }
   }
+}
 
+terraform {
   backend "s3" {
     bucket = "dineshdevops.online"
     key    = "test-ec2_modules"
     region = "us-east-1"
     dynamodb_table = "dineshdevops.online-locking"
   }
-
-  provider "aws" {
-    region = "us-east-1"
-  }
 }
+
+provider "aws" {
+    region = "us-east-1"
+ }
