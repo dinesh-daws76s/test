469c0447742874ee6d465737954beb9d811c1f02
dbramanayanam <152831955+dbramanayanam@users.noreply.github.com>
2024-11-20 14:13:46 +0400

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "dineshdevops.online"
+    key    = "test-ec2_modules"
+    region = "us-east-1"
+    dynamodb_table = "dineshdevops.online-locking"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+ }
